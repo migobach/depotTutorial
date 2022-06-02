e7db7ccd5fa07fc7f5f93cc099782bc2dcf5e7a8
Michael Bach <migobach@gmail.com>
2022-06-02 08:28:23 -0700

    init commit on working through rails 7 book

---
+++ test/models/product_test.rb
@@ -0,0 +1,71 @@
+require "test_helper"
+
+class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
+  test "product attribute must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(
+      title: "Book Title",
+      description: "This is a really good book",
+      image_url: "test.jpg"
+    )
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(
+      title: "Book Title Again",
+      description: "Here is another description",
+      price: 1,
+      image_url: image_url
+    )
+  end
+
+  test "image url is valid" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?,
+      "#{image_url} must be valid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?,
+      "#{image_url} must be invalid"
+    end
+  end
+
+  test "product not valid without a unique title" do
+    product = Product.new(
+      title: products(:ruby_book).title,
+      description: "another description",
+      price: 2,
+      image_url: "ruby.jpg"
+    )
+
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+end
